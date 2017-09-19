06cdb7f6a107dfe526d3240952beb22c073be4d2
rookielzy <slimjetlzy@gmail.com>
2017-09-19 12:04:11 +0800

    Preset CSS

---
+++ src/components/StorePicker.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+    render() {
+        return (
+            <form>
+                
+            </form>
+        );
+    }
+}
+
+export default StorePicker;
