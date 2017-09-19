4bbd4081129ccd497b296946aeccd7c0b3a69c11
rookielzy <slimjetlzy@gmail.com>
2017-09-19 13:40:50 +0800

    Header Done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import Header from './components/Header';
+import Order from './components/Order';
+import Inventory from './components/Inventory';
+import './css/style.css';
 
-class Hello extends React.Component {
+class App extends React.Component {
     render() {
         return (
-            <h1>Hello World</h1>
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <Header tagline='Fresh Fish Market'/>
+                </div>
+                <Order />
+                <Inventory />
+            </div>
         );
     }
 }
 
-ReactDOM.render(<Hello />, document.getElementById('main'));
+
+ReactDOM.render(<App />, document.getElementById('main'));
 registerServiceWorker();
