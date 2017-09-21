40519c777f242bbf9b22c68c56260d3e0c6b3f26
rookielzy <slimjetlzy@gmail.com>
2017-09-21 19:59:10 +0800

    About State

--- src/index.js
+++ src/index.js
@@ -10,17 +10,59 @@ import StorePicker from './components/StorePicker';
 import Header from './components/Header';
 import Order from './components/Order';
 import Inventory from './components/Inventory';
+import Fish from './components/Fish';
 import './css/style.css';
+import sampleFishes from './sample-fishes';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fishes: {},
+            order: {}
+        };
+        this.addFish = this.addFish.bind(this);
+        this.loadSamples = this.loadSamples.bind(this);
+        this.addToOrder = this.addToOrder.bind(this);
+    }
+
+    addFish(fish) {
+        const fishes = {...this.state.fishes};
+        const timestamp = Date.now();
+        fishes[`fish-${timestamp}`] = fish;
+        this.setState({ fishes });
+    }
+
+    loadSamples() {
+        this.setState({
+            fishes: sampleFishes
+        });
+    }
+
+    addToOrder(key) {
+        const order = {...this.state.order};
+        order[key] = order[key] + 1 || 1;
+        this.setState({ order });
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline='Fresh Fish Market'/>
+                    <ul className="list-of-fishes">
+                        {
+                            Object.keys(this.state.fishes).map(key => 
+                                    <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder}/>
+                            )
+                        }
+                    </ul>
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory
+                    addFish={this.addFish}
+                    loadSamples={this.loadSamples}
+                />
             </div>
         );
     }
